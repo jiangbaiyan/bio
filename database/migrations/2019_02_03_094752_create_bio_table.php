be18a6de3dce2fa7b055e1742ee1822e99ee5794
jiangbaiyan <987082332@qq.com>
2019-02-03 13:44:27 +0800

    修复bug

--- database/migrations/2019_02_03_094752_create_bio_table.php
+++ database/migrations/2019_02_03_094752_create_bio_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReserveTable extends Migration
+class CreateBioTable extends Migration
 {
     /**
      * Run the migrations.
